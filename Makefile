5e0b55f1ebe99fb4af7979cf19935a5cff3b6f57
greatofdram <greatofdream@gmail.com>
2019-11-27 16:57:01 +0800

    preprocessing the data finished

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 .PHONY: all
 all:
+
+.PHONY: changeFormat
+changeFormat:
+	iconv -f gbk -t utf-8 data/buildTable.sql -o data/buildTableU.sql
+.DELETE_ON_ERROR:
+.SECONDARY:
--- src/rfm.py
+++ src/rfm.py
@@ -1,5 +1,23 @@
+#! /usr/bin/python3
 import re
 
 def extractHeader(sqlFile):
    fsql = open(sqlFile)
-   for i in fsql.read()
+   regexString = r"'(.*)'"
+   name = []
+   store = []
+   for line in fsql:
+      if re.search(r'\(', line):
+         beginFlag = 1
+      elif re.search(r'\)', line):
+         beginFlag = 0
+      if beginFlag ==1:
+         matchObj =re.search(regexString, line)
+         if matchObj:
+            name.append(matchObj.group(1))
+      else:
+         matchObj =re.search(regexString, line)
+         if matchObj:
+            store.append({matchObj.group(1): name})
+            name =[]
+   return store
